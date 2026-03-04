acceb475989beb1264e5ede93cc9b85f3419b332
Anike Victor <jchukwuebuka194@gmail.com>
2026-03-04 22:04:18 +0100

    Getting Human Choice I wrote a code prompting the user to input Rock, Paper or Scissors and returning the prompt

--- javascript.js
+++ javascript.js
@@ -2,4 +2,12 @@ let getComputerChoice = function () {randomNumber = Math.floor(Math.random() * 3
 if (randomNumber === 0) {return "Rock"}
 else if (randomNumber === 1) {return "Paper"}
 else {return "Scissors"}};
-alert(getComputerChoice());
+let getHumanChoice = function () {userInput = prompt("Rock, Paper or Scissors")
+    if (userInput === "Rock") {return "Rock"}
+    else if (userInput === "Paper") {return "Paper"}
+    else if (userInput === "Scissors") {return "Scissors"}
+    else {return "Are you mad or something?"}
+};
+alert(getHumanChoice());
+let humanScore = 0;
+let computerScore = 0;
